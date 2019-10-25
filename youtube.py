7f35c2609acd521a99b174a2b992c2222406d0ee
Lucas Rodrigues Severo <lucas_severo@outlook.com.br>
2019-10-25 11:12:18 -0400

    v.1.1

--- youtube.py
+++ youtube.py
@@ -4,7 +4,7 @@ import math
 
 token = "yourToken"
 
-url = "Playlist Link"
+url = "PlaylistLink"
 index = url.find("list=")
 
 playlistId = url[index+5:]
@@ -22,28 +22,27 @@ try:
 		
 	print(f"playlistLink: https://www.youtube.com/playlist?list={response[0]['snippet']['playlistId']}\n")
 
-	if pages >= 1:
-		## pega 50 vídeos para cada página que existir na playlist
-		while page <= pages:
-			try:
-				nextPage = r['nextPageToken']
-				url = f"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&pageToken={nextPage}&playlistId={playlistId}&key={token}"
-			except:
-				print()
+	## pega 50 vídeos para cada página que existir na playlist
+	while page <= pages:
+		try:
+			nextPage = r['nextPageToken']
+			url = f"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&pageToken={nextPage}&playlistId={playlistId}&key={token}"
+		except:
+			print()
 
-			for item in response:
-				title = item['snippet']['title']
-				if title != "Private video":
-					print(f"Video {cont}")
-					print(f"Title: {item['snippet']['title']}")
-					print(f"publishedAt: {item['snippet']['publishedAt']}")
-					print(f"Link: https://www.youtube.com/watch?v={item['snippet']['resourceId']['videoId']}")
-					print(f"Thumbnail: {item['snippet']['thumbnails']['high']['url']}\n")
-				cont += 1
+		for item in response:
+			title = item['snippet']['title']
+			if title != "Private video":
+				print(f"Video {cont}")
+				print(f"Title: {item['snippet']['title']}")
+				print(f"publishedAt: {item['snippet']['publishedAt']}")
+				print(f"Link: https://www.youtube.com/watch?v={item['snippet']['resourceId']['videoId']}")
+				print(f"Thumbnail: {item['snippet']['thumbnails']['high']['url']}\n")
+			cont += 1
 
-			r = requests.get(url).json()
-			response = r['items']
-			page += 1
+		r = requests.get(url).json()
+		response = r['items']
+		page += 1
 except Exception as e:
 	print("Connection Error")
 
